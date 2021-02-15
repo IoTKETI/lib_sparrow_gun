137fb5d537a48d3f76c71f40be460485c0f6bd4f
dnjstjr93 <dnjstjr93@gmail.com>
2021-02-15 18:46:14 +0900

    - edit json path (msw directory)

--- lib_sparrow_gun.py
+++ lib_sparrow_gun.py
@@ -184,7 +184,7 @@ def main():
 
     try:
         lib = dict()
-        with open('./' + my_lib_name + '/' + my_lib_name + '.json', 'r') as f:
+        with open('./' + my_msw_name + '/' + my_lib_name + '.json', 'r') as f:
             lib = json.load(f)
             lib = json.loads(lib)
 
@@ -199,7 +199,7 @@ def main():
         lib = json.dumps(lib, indent=4)
         lib = json.loads(lib)
 
-        with open('./' + my_lib_name + '/' + my_lib_name + '.json', 'w', encoding='utf-8') as json_file:
+        with open('./' + my_msw_name + '/' + my_lib_name + '.json', 'w', encoding='utf-8') as json_file:
             json.dump(lib, json_file, indent=4)
 
     lib['serialPortNum'] = argv[1]
